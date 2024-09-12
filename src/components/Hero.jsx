143420362f7566d728dba4b4a6e506a11bedc34f
basit <hassanbasit1990@gmail.com>
2024-09-12 12:21:21 +0100

    Added changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,20 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 const Hero = () => {
-  const [username, setUsername] = useState("Mate"); // default fallback username
+  const [username, setUsername] = useState("Mate");
 
   useEffect(() => {
-    // Ensure the Telegram WebApp is available
     if (window.Telegram && window.Telegram.WebApp) {
-      // Notify Telegram that the app is ready
       window.Telegram.WebApp.ready();
 
-      // Try to get the username from initDataUnsafe
       if (
         window.Telegram.WebApp.initDataUnsafe &&
         window.Telegram.WebApp.initDataUnsafe.user
       ) {
         const telegramUser = window.Telegram.WebApp.initDataUnsafe.user;
-        setUsername(telegramUser.username || "Mate"); // Extracting the username from the user object
+        setUsername(telegramUser.username || "Mate");
       } else {
-        console.warn("Telegram WebApp did not provide initDataUnsafe.user");
+        console.error("Telegram WebApp did not provide initDataUnsafe.user");
       }
     } else {
       console.error("Telegram WebApp not available");
