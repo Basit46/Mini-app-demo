461668e6157b819d97c03f3d8ea682d96ef23cf2
basit <hassanbasit1990@gmail.com>
2024-09-12 11:14:39 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="font-ChelseaMarket">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
