f78c58bfbce224659cb3d9736b5185adcf140b86
basit <hassanbasit1990@gmail.com>
2024-09-12 11:43:07 +0100

    Added changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Navbar from "./Navbar";
 
 const Hero = () => {
+  console.log(window.Telegram.WebApp.initData.username);
   return (
     <div className="hero relative h-screen">
       <Navbar />
+
+      <div>Username: {window.Telegram.WebApp.initData.username || "Mate"}</div>
     </div>
   );
 };
