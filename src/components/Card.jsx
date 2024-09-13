b65a624b58fa5a3664f34fd530bb9bc8d40bf679
basit <hassanbasit1990@gmail.com>
2024-09-13 15:48:37 +0100

    Updated tapping

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Card = () => {
   const [clicks, setClicks] = useState([]);
-  const [points, setPoints] = useState(0);
+  const [points, setPoints] = useState(() => {
+    const storedPoints = localStorage.getItem("points");
+    return storedPoints ? parseFloat(storedPoints) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("points", JSON.stringify(points));
+  }, [points]);
+
+  const [supply, setSupply] = useState(5000);
 
   const handleClick = (e) => {
-    setPoints(points + 4);
+    if (supply - 4 > 0) {
+      setPoints(points + 4);
+      setSupply(supply - 4);
 
-    const cardRect = e.currentTarget.getBoundingClientRect(); // Get card's position and size
-    const x = e.clientX - cardRect.left; // Calculate the x position relative to the card
-    const y = e.clientY - cardRect.top; // Calculate the y position relative to the card
+      const cardRect = e.currentTarget.getBoundingClientRect(); // Get card's position and size
+      const x = e.clientX - cardRect.left; // Calculate the x position relative to the card
+      const y = e.clientY - cardRect.top; // Calculate the y position relative to the card
 
-    const newClick = {
-      id: uuidv4(), // Unique ID for each click
-      x,
-      y,
-    };
-    setClicks([...clicks, newClick]);
+      const newClick = {
+        id: uuidv4(), // Unique ID for each click
+        x,
+        y,
+      };
+      setClicks([...clicks, newClick]);
 
-    // Remove the click effect after the animation completes (2s)
-    setTimeout(() => {
-      setClicks((clicks) => clicks.filter((click) => click.id !== newClick.id));
-    }, 2000);
+      // Remove the click effect after the animation completes (2s)
+      setTimeout(() => {
+        setClicks((clicks) =>
+          clicks.filter((click) => click.id !== newClick.id)
+        );
+      }, 2000);
+    } else {
+      alert("Supply finished");
+    }
   };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSupply((prevSupply) => {
+        if (prevSupply < 5000) {
+          return prevSupply + 1;
+        } else {
+          return prevSupply;
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div className="flex flex-col gap-[20px] justify-center items-center">
@@ -47,6 +78,16 @@ const Card = () => {
       </div>
 
       <p>Points: {points}</p>
+
+      <div>
+        <div className="relative w-[200px] h-[10px] bg-gray-300 rounded-[20px] overflow-hidden">
+          <div
+            style={{ width: (supply / 5000) * 100 + "%" }}
+            className="absolute left-0 top-0 h-full bg-[red]"
+          />
+        </div>
+        <p>{supply}/5000</p>
+      </div>
     </div>
   );
 };
